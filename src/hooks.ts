70a6a2d70573b78fc2b97a9fd376378392e5bca4
Anwesh43 <anweshthecool0@gmail.com>
2022-04-14 16:35:32 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const scGap : number = 0.01 
 const delay : number = 20
@@ -37,12 +37,69 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     }, [])
     return {
         w, 
         h, 
+    }
+}
+const sinify : Function = (scale : number) => Math.sin(scale * Math.PI)
+
+export const useStyle = (scale : number, w : number, h : number) => {
+    const sf : number = sinify(scale)
+    const r : number = Math.min(w, h) / 15 
+    const l : number = Math.min(w, h) / 3 
+    const position = 'absolute'
+    const borderRadius = '50%'
+    return {
+        parentStyle() : CSSProperties {
+            const left = `${w / 2}px`
+            const top = `${h / 2}px`
+            return {
+                position, 
+                left, 
+                top 
+            }
+        },
+        circleOrbitStyle(i : number, n : number) : CSSProperties {
+            const deg : number = 2 * Math.PI / n 
+            const x : number = l * sf * Math.cos(-deg / 2 + deg * i)
+            const y : number = l * sf * Math.sin(-deg / 2 + deg * i)
+            const left : string = `${x - r / 2}px`
+            const top : string = `${y - r / 2}px`
+            const width : string = `${r}px`
+            const height : string = `${r}px`
+            const background : string = "#00C853"
+            return {
+                position, 
+                left, 
+                top, 
+                width, 
+                height,  
+                opacity: sf,
+                borderRadius, 
+                background 
+            }
+        }, 
+
+        circleFixedStyle() : CSSProperties  {
+            const left = `${-r / 2}px`
+            const top = `${-r / 2}px`
+            const width = `${-r / 2}px`
+            const height = `${-r / 2}px`
+            const background : string = '#304FFE'
+            return {
+                position, 
+                left, 
+                top, 
+                width, 
+                height, 
+                borderRadius,
+                background 
+            }
+        }
     }
 }
